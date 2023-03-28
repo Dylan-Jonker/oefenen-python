74a49e1ad6944a97f93b7de4a9ec3cb4d3d8227c
Dylan-Jonker <112872251+Dylan-Jonker@users.noreply.github.com>
2023-03-28 08:54:46 +0200

    .

---
+++ oefenen/if_statements.py
@@ -0,0 +1,14 @@
+tempreture = 25
+if tempreture < 30:
+    print("it's a hot day")
+    print("drink plenty of water")
+elif tempreture < 20: # (20, 30]
+    print("it's a nice day")
+elif tempreture < 10: # (10, 20]
+    print("it's a cold day")
+else:
+    print("it's cold")
+print("done")
+
+
+
